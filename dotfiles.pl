562c916bc1f2d89d57eab360494f3301ebb32046
nukosuke <39.nuko@gmail.com>
2018-02-15 09:08:40 +0000

    fix help msg

--- dotfiles.pl
+++ dotfiles.pl
@@ -45,6 +45,8 @@ Subcommands:
 	link		-- create symlinks to HOME
 	dry_link	-- output symlink command, but no execution
 	unlink		-- remove all symlinks from HOME
+	dry_unlink	-- output symlink delete command, but no execution
+
 EOS
 	;
 	print($help);
