f040c4a60adbdd6390c2ed5dc4fa16529f5074ec
nukosuke <39.nuko@gmail.com>
2018-02-16 10:12:33 +0000

    add helm keymap

--- .emacs.d/init/040_helm.el
+++ .emacs.d/init/040_helm.el
@@ -1,3 +1,8 @@
 (use-package helm :straight t)
 (use-package helm-config)
 (helm-mode 1)
+
+(define-key global-map (kbd "C-x C-f") 'helm-find-files)
+(define-key global-map (kbd "C-x b") 'helm-buffers-list)
+(define-key helm-map (kbd "C-h") 'delete-backward-char)
+
