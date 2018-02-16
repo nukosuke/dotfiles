d645645011ef749e866425a78daedea48907d6b6
nukosuke <39.nuko@gmail.com>
2018-02-16 09:20:06 +0000

    rename & add pkgs

--- .emacs.d/init/150_lang-modes.el
+++ .emacs.d/init/150_lang-modes.el
@@ -1,13 +1,20 @@
-;;; auto-mode-alist.el -- メジャーモードの起動設定
+;;; lang-modes.el -- 言語モードの設定
 ;;; Commentary:
 ;;; Code:
 
+(use-package web-mode :straight t)
 (add-to-list 'auto-mode-alist '("\\.html\\'" . web-mode))
 (add-to-list 'auto-mode-alist '("\\.css\\'" . web-mode))
 (add-to-list 'auto-mode-alist '("\\.php\\'" . web-mode))
-(add-to-list 'auto-mode-alist '("\\.js\\'" . js2-mode))
 (add-to-list 'auto-mode-alist '("\\.jsx\\'" . web-mode))
-(add-to-list 'auto-mode-alist '("\\.scala\\'" .scala-mode))
+
+(use-package js2-mode :straight t)
+(add-to-list 'auto-mode-alist '("\\.js\\'" . js2-mode))
+
+(use-package ruby-mode :straight t)
+(use-package go-mode :straight t)
+(use-package yaml-mode :straight t)
+
 (provide 'auto-mode-alist)
 
 ;;; auto-mode-alist.el ends here
