e2f78656b5b880e645758e3ad2d8f06834d88f7e
nukosuke <nukosuke@shibuya.tech>
2018-02-19 02:27:54 +0900

    add which-key

---
+++ .emacs.d/init/006_which-key.el
@@ -0,0 +1,10 @@
+;;; 006_which-key.el -- キーバインドのガイド表示設定
+;;; Commentary:
+;;; Code:
+
+(use-package which-key)
+(which-key-setup-side-window-right)
+(which-key-mode 1)
+
+(provide '006_which-key)
+;;; 006_which-key.el ends here
