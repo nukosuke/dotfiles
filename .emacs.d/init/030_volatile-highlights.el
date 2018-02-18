1f44ad74ae75a071951318989adf94bec3d638a7
nukosuke <nukosuke@shibuya.tech>
2018-02-19 01:48:43 +0900

    add volatile-highlights

---
+++ .emacs.d/init/030_volatile-highlights.el
@@ -0,0 +1,9 @@
+;;; 030_volatile-highlights.el -- killやyank時の強調表示設定
+;;; Commentary:
+;;; Code:
+
+(use-package volatile-highlights)
+(volatile-highlights-mode t)
+
+(provide '030_volatile-highlights)
+;;; 030_volatile-highlights.el ends here
