c64cedf0fa519661e2f90e212cccd09bb3bf1dda
nukosuke <nukosuke@shibuya.tech>
2018-02-19 00:42:23 +0900

    fix helm config

--- .emacs.d/init/040_helm.el
+++ .emacs.d/init/040_helm.el
@@ -1,8 +1,14 @@
+;;; 040_helm.el -- helmの設定
+;;; Commentary:
+;;; Code:
+
 (use-package helm)
-(use-package helm-config)
+(require 'helm-config)
 (helm-mode 1)
 
 (define-key global-map (kbd "C-x C-f") 'helm-find-files)
 (define-key global-map (kbd "C-x b") 'helm-buffers-list)
 (define-key helm-map (kbd "C-h") 'delete-backward-char)
 
+(provide '040_helm)
+;;; 040_helm.el ends here
