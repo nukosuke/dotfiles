1c5241f03c85ff2a46667ccc4b00a0a80d8dbf9e
nukosuke <nukosuke@shibuya.tech>
2018-02-19 00:43:22 +0900

    refactor

--- .emacs.d/init/001_general.el
+++ .emacs.d/init/001_general.el
@@ -1,16 +1,18 @@
-;;; general-init.el -- 共通の設定
+;;; 001_general.el -- 共通の設定
 ;;; Commentary:
 ;;; Code:
 (custom-set-variables
  '(current-language-environment "UTF-8")
- ;; スタートアップ画面を表示しない(scratchバッファを表示)
- '(inhibit-startup-screen t)
  ;; カーソルホバー時に括弧の対応を表示する
  '(show-paren-mode t)
  ;; タイトルバーフォーマット
  '(frame-title-format (format "%%f" (system-name)))
  ;; バックアップファイル(*~)を作成しない
- '(make-backup-files nil))
+ '(make-backup-files nil)
+ ;; タブ幅を4に指定
+ '(tab-width 4)
+ ;; タブをスペースに
+ '(indent-tabs-mode nil))
 
 ;; ツールバー非表示
 (tool-bar-mode 0)
@@ -21,17 +23,5 @@
 ;; スクロールバー非表示
 (scroll-bar-mode 0)
 
-;; タブ幅を4に指定
-(setq-default tab-width 4)
-
-;; タブをスペースに
-(setq-default indent-tabs-mode nil)
-
-;; Ctrl-hをバックスペースにする
-(global-set-key (kbd "C-h") 'delete-backward-char)
-
-;; 改行後に自動でインデント
-(global-set-key (kbd "C-m") 'newline-and-indent)
-
-(provide 'general-init)
-;;; general-init.el ends here
+(provide '001_general)
+;;; 001_general.el ends here
--- .emacs.d/init/999_key-bindings.el
+++ .emacs.d/init/999_key-bindings.el
@@ -1,10 +1,19 @@
-;;; key-bindings-init.el -- キーバインドの設定
+;;; 999_key-bindings.el -- キーバインドの設定
 ;;; Commentary:
 ;;; Code:
 
+;; C-a 行頭 -> ファイルの先頭
+;; C-e 行末 -> ファイルの末尾
+(use-package sequential-command)
 (require 'sequential-command-config)
 (global-set-key "\C-a" 'seq-home)
 (global-set-key "\C-e" 'seq-end)
 
-(provide 'key-bindings-init)
-;;; key-bindings-init.el ends here
+;; バックスペース
+(global-set-key (kbd "C-h") 'delete-backward-char)
+
+;; 改行&自動インデント
+(global-set-key (kbd "C-m") 'newline-and-indent)
+
+(provide '999_key-bindings)
+;;; 999_key-bindings.el ends here
