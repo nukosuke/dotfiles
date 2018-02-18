d68be978f8212f89d2e851dd1cbdb33bf30ee79b
nukosuke <nukosuke@shibuya.tech>
2018-02-19 01:42:36 +0900

    add smartparens

---
+++ .emacs.d/init/025_smartparens.el
@@ -0,0 +1,9 @@
+;;; 025_smartparens.el -- 対応する括弧の自動補完設定
+;;; Commentary:
+;;; Code:
+
+(use-package smartparens)
+(smartparens-global-mode t)
+
+(provide '025_smartparens)
+;;; 025_smartparens.el ends here
