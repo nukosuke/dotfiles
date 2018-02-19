0a65548a6e799ef83870d8b0e694d6e9706b6d06
nukosuke <39.nuko@gmail.com>
2018-02-19 06:48:04 +0000

    update helm config

--- .emacs.d/init/040_helm.el
+++ .emacs.d/init/040_helm.el
@@ -2,11 +2,20 @@
 ;;; Commentary:
 ;;; Code:
 
-(use-package helm)
-(require 'helm-config)
-(helm-mode 1)
+(use-package helm
 
-(define-key helm-map (kbd "C-h") 'delete-backward-char)
+  :init
+  (require 'helm-config)
+  ;; 下までいったら上に戻ってくる（逆も同じ）
+  (setq helm-move-to-line-cycle-in-source t)
+  ;; 分割時でも今のウィンドウでhelmバッファを開く
+  (setq helm-split-window-in-side-p t)
+  ;; 常に有効化
+  (helm-mode 1)
+
+  :bind
+  (:map helm-map
+              ("C-h" . delete-backward-char)))
 
 (provide '040_helm)
 ;;; 040_helm.el ends here
