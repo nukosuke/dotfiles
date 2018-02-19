a9e9ae4dd0867cd0fb6811fd489949f84eaeecd7
nukosuke <39.nuko@gmail.com>
2018-02-19 09:01:59 +0000

    refactor with use-package

--- .emacs.d/init/020_hlinum.el
+++ .emacs.d/init/020_hlinum.el
@@ -1,3 +1,10 @@
-(use-package hlinum)
-(global-linum-mode 1)
+;;; 020_hlinum.el -- 行番号の表示設定
+;;; Commentary:
+;;; Code:
 
+(use-package hlinum
+  :init
+  (global-linum-mode 1))
+
+(provide '020_hlinum)
+;;; 020_hlinum.el ends here
--- .emacs.d/init/100_popup.el
+++ .emacs.d/init/100_popup.el
@@ -2,16 +2,15 @@
 ;;; Commentary:
 ;;; Code:
 
-(use-package popup)
-
-;; TabもしくはCtrl+nで次の候補に移動
-(define-key popup-menu-keymap (kbd "TAB") 'popup-next)
-(define-key popup-menu-keymap (kbd "C-n") 'popup-next)
-
-;; Shift+TabもしくはCtrl+pで前の候補に移動
-(define-key popup-menu-keymap (kbd "<backtab>") 'popup-previous)
-(define-key popup-menu-keymap (kbd "C-p") 'popup-previous)
+(use-package popup
+  :bind
+  (:map popup-menu-keymap
+        ;; TabもしくはCtrl+nで次の候補に移動
+        ("TAB" . popup-next)
+        ("C-n" . popup-next)
+        ;; Shift+TabもしくはCtrl+pで前の候補に移動
+        ("<backtab>" . popup-previous)
+        ("C-p" . popup-previous)))
 
 (provide 'popup-init)
-
 ;;; popup-init.el ends here
