e0551ba5d82cdcdc5417ad29704368de494b270a
nukosuke <nukosuke@lavabit.com>
2018-03-14 17:17:54 +0900

    add pretty-mode

---
+++ .emacs.d/init/012_pretty-mode.el
@@ -0,0 +1,10 @@
+;;; 012_pretty-mode.el --- redisplay mode line
+;;; Commentary:
+;;; Code:
+
+(use-package pretty-mode
+  :config
+  (global-pretty-mode t))
+
+(provide '012_pretty-mode)
+;;; 012_pretty-mode.el ends here
