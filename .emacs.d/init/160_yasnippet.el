7b8e879676f6ff714fb3a1c23c42ba7b6a58656d
nukosuke <nukosuke@lavabit.com>
2019-01-10 14:12:44 +0900

    Commenting

--- .emacs.d/init/160_yasnippet.el
+++ .emacs.d/init/160_yasnippet.el
@@ -1,18 +1,24 @@
 ;;; yasnippet.el -- yasnippet初期化
+
 ;;; Commentary:
+;; yasnippetの初期化を行う
+;; companyの補完候補にスニペットを表示する
+;; AndreaCrotti/yasnippet-snippets を読み込み
+
 ;;; Code:
 
 (use-package yasnippet
   :after company
   :diminish yas-minor-mode
+
   :custom
   (yas-snippet-dirs '("~/.emacs.d/snippets"
                       "~/.emacs.d/straight/repos/yasnippet-snippets/snippets") "スニペットのディレクトリ")
 
   :config
   (yas-global-mode 1)
+  ;; companyの連携設定
   (defvar company-mode/enable-yas t)
-
   (defun company-mode/backend-with-yas (backend)
     (if (or (not company-mode/enable-yas) (and (listp backend) (member 'company-yasnippet backend)))
 	    backend
@@ -22,6 +28,7 @@
     (setq company-backends (mapcar #'company-mode/backend-with-yas company-backends)))
   (add-hook 'company-mode-hook 'set-yas-as-company-backend))
 
+;; https://github.com/AndreaCrotti/yasnippet-snippets
 (use-package yasnippet-snippets
   :after yasnippet)
 
