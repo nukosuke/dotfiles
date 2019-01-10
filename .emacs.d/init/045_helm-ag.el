cfd45a911ec43faa76d4590481ae48addc076e84
nukosuke <nukosuke@lavabit.com>
2019-01-10 14:15:30 +0900

    Commenting

--- .emacs.d/init/045_helm-ag.el
+++ .emacs.d/init/045_helm-ag.el
@@ -1,8 +1,12 @@
 ;;; 045_helm-ag.el -- helmでSilverSearcherを使う設定
+
 ;;; Commentary:
+;; C-c a g でagコマンドの実行結果をhelmで絞り込みできるようにする
+
 ;;; Code:
 
 (use-package helm-ag
+  :after helm
   :if (executable-find "ag")
   :bind ("C-c a g" . helm-ag))
 
