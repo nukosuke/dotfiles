63154a16873a591c9ed4eb80b9dd1501b9148475
nukosuke <nukosuke@lavabit.com>
2019-01-10 18:36:39 +0900

    Ignore magit buffer in iflipb

--- .emacs.d/init/120_iflipb.el
+++ .emacs.d/init/120_iflipb.el
@@ -2,10 +2,15 @@
 
 ;;; Commentary:
 ;; M-p M-nでバッファを連続して切り替える
+;; *始まりのバッファやmagitのバッファは切り替え候補には入れない
 
 ;;; Code:
 
 (use-package iflipb
+  :init
+  (setq iflipb-wrap-around t)
+  (setq iflipb-ignore-buffers (list "^[*]" "^magit"))
+  
   :bind (("M-p" . 'iflipb-previous-buffer)
          ("M-n" . 'iflipb-next-buffer)))
 
