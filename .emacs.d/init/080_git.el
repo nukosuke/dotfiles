41fdb50796ebb9082b9caa062f379d3d197fe3fd
nukosuke <39.nuko@gmail.com>
2019-02-10 05:26:10 +0900

    Git hydra (#17)
    * Remove which-key, iflipb
    
    * git hydra

--- .emacs.d/init/080_git.el
+++ .emacs.d/init/080_git.el
@@ -1,27 +1,49 @@
-;;; 080_git.el -- gitの設定
+;;; 080_git.el -- gitの設定 -*- lexical-binding: t -*-
 ;;; Commentary:
 ;;
 ;; magit:
 ;; - gitクライアント
 ;;
+;; git-gutter
+;; - hunk単位の操作用
+;;
 ;; git-gutter-fringe:
 ;; - 変更行にマークを表示
-;; - hunk単位のstage
 ;;
 ;;; Code:
 
+;;
+;; magit
+;;
 (use-package magit
   :bind
-  (("C-c C-c" . magit-dispatch-popup)
-   ("C-c g s" . magit-status)
-   ("C-c g l" . magit-log-all)))
+  ("C-c C-c" . magit-dispatch-popup))
 
-(use-package magit-popup
-  :after magit)
+;;
+;; git-gutter
+;;
+(use-package git-gutter
+  :custom
+  (git-gutter:ask-p nil)
+  :bind
+  ("C-c g" . hydra-git-gutter/body))
 
+;;
+;; git-gutterのhunk操作用hydra
+;;
+(defhydra hydra-git-gutter nil
+  "git hunk"
+  ("p" git-gutter:previous-hunk "previous")
+  ("n" git-gutter:next-hunk "next")
+  ("s" git-gutter:stage-hunk "stage")
+  ("r" git-gutter:revert-hunk "revert")
+  ("m" git-gutter:mark-hunk "mark")
+  ("SPC" git-gutter:popup-hunk "popup viewer"))
+
+;;
+;; git-gutter-fringe
+;;
 (use-package git-gutter-fringe
-  :init
-  (global-git-gutter-mode t)
   :config
   (fringe-helper-define 'git-gutter-fr:added '(center repeated)
     "XXX.....")
@@ -32,6 +54,7 @@
     "XX......"
     "XXX....."
     "XXXX....")
+  (global-git-gutter-mode t)
   :diminish git-gutter-mode)
 
 (provide '080_git)
