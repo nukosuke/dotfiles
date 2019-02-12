d76bb129d37fa293e4bee5b9322b22d535f4e6e3
nukosuke <nukosuke@lavabit.com>
2019-02-12 18:38:28 +0900

    Add tide for TypeScript completion

--- .emacs.d/init/150_lang-modes.el
+++ .emacs.d/init/150_lang-modes.el
@@ -1,27 +1,54 @@
-;;; 150_lang-modes.el -- 言語モードの設定
+;;; 150_lang-modes.el -- 言語モードの設定 -*- lexical-binding: t -*-
 ;;; Commentary:
+;;
+;; TODO: LSPの導入
+;;
 ;;; Code:
 
+;; Shell
 (use-package fish-mode)
+
+;; Web
 (use-package web-mode :mode "\\.html\\'" "\\.css\\'" "\\.php\\'" "\\.jsx\\'")
 (use-package js2-mode :mode "\\.js\\'")
-(use-package typescript-mode)
-(use-package ruby-mode
-  :init
-  (custom-set-variables '(ruby-insert-encoding-magic-comment nil)))
-(use-package elixir-mode)
-(use-package go-mode)
-(use-package rust-mode)
-(use-package lua-mode)
 (use-package json-mode)
+(use-package slim-mode)
+(use-package markdown-mode)
+
+;; Conf
 (use-package yaml-mode)
 (use-package cmake-mode)
 (use-package dockerfile-mode)
-(use-package markdown-mode)
-(use-package slim-mode)
-(use-package protobuf-mode)
-(use-package gitignore-mode)
 (use-package terraform-mode)
+(use-package gitignore-mode)
+
+;; TypeScript
+(use-package typescript-mode)
+(use-package tide
+  :after (typescript-mode company flycheck)
+  :hook ((typescript-mode . tide-setup)
+         (typescript-mode . tide-hl-identifier-mode)
+         (before-save . tide-format-before-save)))
+
+;; Ruby
+(use-package ruby-mode
+  :init
+  (custom-set-variables '(ruby-insert-encoding-magic-comment nil)))
+
+;; Elixit
+(use-package elixir-mode)
+
+;; Go
+(use-package go-mode)
+
+;; Rust
+(use-package rust-mode)
+
+;; Lua
+(use-package lua-mode)
+
+;; etc
+(use-package protobuf-mode)
 
 (provide '150_lang-modes)
 
