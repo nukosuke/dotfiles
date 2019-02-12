5d2c7db76f59b02e9aa2815e2ffe47f2c115098e
nukosuke <nukosuke@lavabit.com>
2019-02-12 11:05:18 +0900

    Add git-gutter:diff rule for shackle

--- .emacs.d/init/095_popwin.el
+++ .emacs.d/init/095_popwin.el
@@ -1,11 +1,12 @@
-;;; 095_popwin.el -- ポップウィンドウの設定
-
+;;; 095_popwin.el -- ポップウィンドウの設定 -*- lexical-binding: t -*-
+;;
 ;;; Commentary:
+;;
 ;; 別バッファを入れ替えず一時的に新しいバッファをポップアップする
-
+;;
 ;; TODO:
 ;; file番号を最初の方に移す
-
+;;
 ;;; Code:
 
 (use-package shackle
@@ -16,10 +17,12 @@
           '(("*Wargings*" :same t)
             ("\*Buffer List" :regexp t :select t :align t :size 0.4)
             ("\\`\\*helm.*?\\*\\'" :regexp t :align t :size 0.4)
-            ("\\*eshell\\*" :regexp t :popup t :select t :align t :size 0.2)))
+            ("\\*eshell\\*" :regexp t :popup t :select t :align t :size 0.2)
+            ("*git-gutter:diff*" :popup t :select nil :align t :size 0.4)))
     (shackle-mode)
     (winner-mode)
     (global-set-key (kbd "C-z") 'winner-undo)))
 
 (provide '095_popwin)
+
 ;;; 095_popwin.el ends here
