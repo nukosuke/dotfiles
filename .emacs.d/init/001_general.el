03e52d709357440f3c7a1c563c6b5d4e157f882b
nukosuke <nukosuke@lavabit.com>
2019-02-21 18:12:29 +0900

    Use transient to define straight.el helper

--- .emacs.d/init/001_general.el
+++ .emacs.d/init/001_general.el
@@ -10,9 +10,11 @@
  ;; バックアップファイル(*~)を作成しない
  '(make-backup-files nil)
  ;; タブ幅を4に指定
- '(tab-width 4)
+ '(tab-width 2)
  ;; タブをスペースに
- '(indent-tabs-mode nil))
+ '(indent-tabs-mode nil)
+ ;; kill & yankにクリップボードを使う
+ '(x-select-enable-clipboard t))
 
 ;; ツールバー非表示
 (tool-bar-mode 0)
@@ -40,8 +42,49 @@
 ;; hydra
 (use-package hydra)
 
+;; transient
+;; is also in magit
+(use-package transient)
+
+;; straight.el helper
+;; transient version of https://github.com/abo-abo/hydra/wiki/straight.el
+(define-transient-command straight-dispatch ()
+  "Invoke a straight.el command from a list of available commands."
+  ["straight.el commands\n"
+   ["Check"
+    ("c" "Check all"         straight-check-all)
+    ("C" "Check package"     straight-check-package)]
+   ["Fetch"
+    ("f" "Fetch all"         straight-fetch-all)
+    ("F" "Fetch package"     straight-fetch-package)]
+   ["Merge"
+    ("m" "Merge all"         straight-merge-all)
+    ("M" "Merge package"     straight-merge-package)]
+   ["Normalize"
+    ("n" "Normalize all"     straight-normalize-all)
+    ("N" "Normalize package" straight-normalize-package)]
+   ["Push"
+    ("u" "Push all"          straight-push-all)
+    ("U" "Push package"      straight-push-package)]]
+  [["Rebuild"
+    ("r" "Rebuild all"       straight-rebuild-all)
+    ("R" "Rebuild package"   straight-rebuild-package)]
+   ["Pull"
+    ("p" "Pull all"          straight-pull-all)
+    ("P" "Pull package"      straight-pull-package)]
+   ["Version"
+    ("v" "Versions freeze"   straight-freeze-versions)
+    ("V" "Versions thaw"     straight-thaw-versions)]
+   ["Watcher"
+    ("w" "Watcher start"     straight-watcher-start)
+    ("W" "Watcher stop"      straight-watcher-stop)]]
+  ["Other"
+   ("g" "Get recipe"         straight-get-recipe)
+   ("e" "Prune build"        straight-prune-build)])
+
 ;; TODO
-;; move to other file
+;; - move to other file
+;; - replace by transient
 (defhydra hydra-zoom (global-map "<f2>")
   "zoom"
   ("g" text-scale-increase "in")
